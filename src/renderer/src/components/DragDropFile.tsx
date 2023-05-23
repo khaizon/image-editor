81ba4aaf7bdd4e7329139d9576ae483d4231eb7e
Kai Feng <kaifeng_toh@mymail.sutd.edu.sg>
2023-05-23 16:43:22 +0800

    can drag and drop files and show file names

---
+++ src/renderer/src/components/DragDropFile.tsx
@@ -0,0 +1,70 @@
+import { ChangeEvent, DragEvent, SyntheticEvent, useRef, useState, FunctionComponent, Dispatch } from 'react'
+import './DragDropFile.css'
+
+type Props = {
+  dispatch: Dispatch<{ type: string; payload: Array<File> }>
+}
+
+const DragDropFile: FunctionComponent<Props> = ({ dispatch }) => {
+  const [dragActive, setDragActive] = useState(false)
+  // ref
+  const inputRef = useRef<null | HTMLInputElement>(null)
+  const handleDrag = function (e: SyntheticEvent): void {
+    e.preventDefault()
+    e.stopPropagation()
+    if (e.type === 'dragenter' || e.type === 'dragover') {
+      setDragActive(true)
+    } else if (e.type === 'dragleave') {
+      setDragActive(false)
+    }
+  }
+
+  const handleDrop = function (e: DragEvent<HTMLDivElement>): void {
+    e.preventDefault()
+    e.stopPropagation()
+    setDragActive(false)
+    if (e.dataTransfer?.files && e.dataTransfer.files[0]) {
+      // at least one file has been dropped so do something
+      // handleFiles(e.dataTransfer.files);
+      dispatch({
+        type: 'add',
+        payload: Array.from(e.dataTransfer.files)
+      })
+    }
+  }
+
+  // triggers when file is selected with click
+  const handleChange = function (e: ChangeEvent<HTMLInputElement>): void {
+    e.preventDefault()
+    if (e.target.files && e.target.files[0]) {
+      // at least one file has been selected so do something
+      // handleFiles(e.target.files);
+      dispatch({
+        type: 'add',
+        payload: Array.from(e.target.files)
+      })
+    }
+  }
+
+  const onButtonClick = (): void => {
+    console.log('click', inputRef.current)
+    inputRef.current?.click()
+  }
+
+  return (
+    <form id="form-file-upload" onDragEnter={handleDrag} onSubmit={(e): void => e.preventDefault()}>
+      <input ref={inputRef} type="file" id="input-file-upload" multiple={true} onChange={handleChange} />
+      <label id="label-file-upload" htmlFor="input-file-upload" className={dragActive ? 'drag-active' : ''}>
+        <div>
+          <p>Drag and drop your file here or</p>
+          <button className="upload-button" onClick={onButtonClick}>
+            Upload a file
+          </button>
+        </div>
+      </label>
+      {dragActive && <div id="drag-file-element" onDragEnter={handleDrag} onDragLeave={handleDrag} onDragOver={handleDrag} onDrop={handleDrop}></div>}
+    </form>
+  )
+}
+
+export default DragDropFile
